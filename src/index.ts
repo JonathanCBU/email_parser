c0c3096bf31cef71d8a0e6284aa7199fa460c217
Jonathan Cook <jcookbme@gmail.com>
2024-02-22 21:37:43 -0500

    Get unique values in CSV column

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,21 @@ import {ImapFlow} from 'imapflow';
 import {parse} from 'csv-parse';
 import * as fs from 'fs';
 import * as path from 'path';
+import {error} from 'console';
+import {PrivateKeyInput} from 'crypto';
 require('dotenv').config();
 
 const email_addr = process.env.EMAIL_ADDR!;
 const email_pwd = process.env.APP_PWD!;
 const data_csv = process.env.APP_TRACKER!;
 
-type AppInfo = {
+interface AppInfo {
   title: string;
   company: string;
   link: string;
   status: number;
   date: Date;
-};
+}
 
 const client = new ImapFlow({
   host: 'imap.gmail.com',
@@ -26,6 +28,31 @@ const client = new ImapFlow({
     pass: email_pwd,
   },
 });
+
+const getRecords = async (): Promise<AppInfo[]> => {
+  let headers = ['title', 'company', 'link', 'status', 'date'];
+  let records: AppInfo[] = [];
+  let parser = fs
+    .createReadStream(path.resolve(data_csv))
+    .pipe(parse({delimiter: ',', columns: headers}));
+  for await (let record of parser) {
+    records.push(record);
+  }
+  return records;
+};
+
+const getUnique = async (
+  records: AppInfo[],
+  column: keyof AppInfo
+): Promise<string[]> => {
+  let col_arr: any[] = [];
+  records.forEach(record => {
+    if (record[column] && !col_arr.includes(record[column])) {
+      col_arr.push(record[column]);
+    }
+  });
+  return col_arr;
+};
 
 const main = async () => {
   // // Wait until client connects and authorizes
@@ -48,27 +75,12 @@ const main = async () => {
   //   lock.release();
   // }
 
-  // log out and close connection
-  await client.logout();
+  // // log out and close connection
+  // await client.logout();
 
-  const csvFilePath = path.resolve(data_csv);
-
-  const headers = ['title', 'company', 'link', 'status', 'date'];
-
-  const fileContent = fs.readFileSync(csvFilePath, {encoding: 'utf-8'});
-
-  parse(
-    fileContent,
-    {
-      delimiter: ',',
-      columns: headers,
-    },
-    (error, result: AppInfo[]) => {
-      if (error) {
-        console.error(error);
-      }
-      console.log('Result', result);
-    }
-  );
+  const data = await getRecords();
+  console.log(data);
+  const comps = await getUnique(data, 'company');
+  console.log(comps);
 };
 main();
