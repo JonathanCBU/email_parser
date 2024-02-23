1ee8b8041da7b65e7facfbf2e5359734769b151b
Jonathan Cook <jcookbme@gmail.com>
2024-02-22 19:10:19 -0500

    Remove JS and add CSV parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,74 @@
-import {mailbox} from './mailbox';
+import {ImapFlow} from 'imapflow';
+import {parse} from 'csv-parse';
+import * as fs from 'fs';
+import * as path from 'path';
 require('dotenv').config();
 
 const email_addr = process.env.EMAIL_ADDR!;
 const email_pwd = process.env.APP_PWD!;
+const data_csv = process.env.APP_TRACKER!;
 
-const emailClient = new mailbox(email_addr, email_pwd);
+type AppInfo = {
+  title: string;
+  company: string;
+  link: string;
+  status: number;
+  date: Date;
+};
 
-console.log(emailClient.client);
+const client = new ImapFlow({
+  host: 'imap.gmail.com',
+  port: 993,
+  secure: true,
+  logger: false,
+  auth: {
+    user: email_addr,
+    pass: email_pwd,
+  },
+});
+
+const main = async () => {
+  // // Wait until client connects and authorizes
+  // await client.connect();
+
+  // // Select and lock a mailbox. Throws if mailbox does not exist
+  // let lock = await client.getMailboxLock('INBOX');
+  // try {
+  //   for await (let message of client.fetch('1:*', {
+  //     uid: true,
+  //     envelope: true,
+  //   })) {
+  //     //   console.log(`UID: ${message.uid}`);\
+  //     console.log(
+  //       'UID: ' + message.uid + ' --- Subject: ' + message.envelope.subject
+  //     );
+  //   }
+  // } finally {
+  //   // Make sure lock is released, otherwise next `getMailboxLock()` never returns
+  //   lock.release();
+  // }
+
+  // log out and close connection
+  await client.logout();
+
+  const csvFilePath = path.resolve(data_csv);
+
+  const headers = ['title', 'company', 'link', 'status', 'date'];
+
+  const fileContent = fs.readFileSync(csvFilePath, {encoding: 'utf-8'});
+
+  parse(
+    fileContent,
+    {
+      delimiter: ',',
+      columns: headers,
+    },
+    (error, result: AppInfo[]) => {
+      if (error) {
+        console.error(error);
+      }
+      console.log('Result', result);
+    }
+  );
+};
+main();
